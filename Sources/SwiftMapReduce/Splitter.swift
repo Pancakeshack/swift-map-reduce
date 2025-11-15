443848752867cd7e3beedac67c0cadc1d1c71874
Austin Townsend <130945920+Pancakeshack@users.noreply.github.com>
2025-11-14 23:02:56 -0700

    add file splitter

---
+++ Sources/SwiftMapReduce/Splitter.swift
@@ -0,0 +1,60 @@
+import Foundation
+
+struct Splitter {
+    let fileManager = FileManager.default
+    let bufferSize: Int = 1024 * 1024
+
+    @available(macOS 12.0, *)
+    func split(filePath: String, numberOfSplits: Int = 4) async throws -> [URL] {
+        let fileUrl = URL(fileURLWithPath: filePath)
+
+        let attributes = try fileManager.attributesOfItem(atPath: fileUrl.path)
+        guard let fileSize = attributes[.size] as? UInt64 else {
+            throw "Unable to determine file size."
+        }
+
+        // Remainder will be included in final chunk
+        let chunkSize = fileSize / UInt64(numberOfSplits)
+        let tmpDirectory = fileManager.temporaryDirectory
+
+        let reader = try FileHandle(forReadingFrom: fileUrl)
+        defer { reader.closeFile() }
+
+        var createdFiles: [URL] = []
+        var writers: [FileHandle] = []
+        for i in 0..<numberOfSplits {
+            let chunkFileUrl = tmpDirectory.appendingPathComponent("chunk.part\(i + 1)")
+            fileManager.createFile(atPath: chunkFileUrl.path, contents: nil)
+            let chunkFileHandle = try FileHandle(forWritingTo: chunkFileUrl)
+            createdFiles.append(chunkFileUrl)
+            writers.append(chunkFileHandle)
+        }
+
+        var currentPart = 1
+        var bytesInCurrentPart: UInt64 = 0
+        var curHandler = writers[currentPart - 1]
+        var buffer = Data(capacity: bufferSize)
+
+        for try await chunk in reader.bytes {
+            buffer.append(chunk)
+            if buffer.count == bufferSize || UInt64(buffer.count) + bytesInCurrentPart >= chunkSize
+            {
+                curHandler.write(buffer)
+                bytesInCurrentPart += UInt64(buffer.count)
+                buffer.removeAll()
+            }
+            if bytesInCurrentPart >= chunkSize && currentPart < numberOfSplits {
+                curHandler = writers[currentPart]
+                currentPart += 1
+                bytesInCurrentPart = 0
+            }
+        }
+
+        curHandler.write(buffer)
+        for writer in writers {
+            writer.closeFile()
+        }
+
+        return createdFiles
+    }
+}
---
+++ Sources/SwiftMapReduce/SwiftMapReduce.swift
@@ -0,0 +1,13 @@
+// The Swift Programming Language
+// https://docs.swift.org/swift-book
+
+@main
+enum SwiftMapReduce {
+    static func main() async {
+        print("Hello, world!")
+    }
+}
+
+extension String: Error {
+
+}
